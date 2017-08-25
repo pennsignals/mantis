1f1ba8d83dc3b4c48c6663fbfd71ca34d4c9ae12
rightlag <rightlag@gmail.com>
2017-08-25 12:06:01 -0400

    Add handler module

---
+++ mantis/handler.py
@@ -0,0 +1,41 @@
+import logging
+
+
+class Component:
+
+    pass
+
+
+class RequestHandler:
+
+    def before_request(self, request):
+        """Runs any additional processing logic on the specified request
+        (before it is executed by the client runtime).
+        """
+        raise NotImplementedError()
+
+    def after_response(self, request, response):
+        """Runs any additional processing logic on the specified request
+        (after is has been executed by the client runtime).
+        """
+        raise NotImplementedError()
+
+    def after_error(self, request, response, e):
+        """Runs any additional processing logic on a request after
+        it has failed.
+        """
+        raise NotImplementedError()
+
+
+class DefaultRequestHandler(RequestHandler):
+
+    DEFAULT_CHARACTER_ENCODING = 'UTF-8'
+
+    def before_request(self, request):
+        pass
+
+    def after_response(self, request, response):
+        pass
+
+    def after_error(self, request, response, e):
+        logging.error(e)
