c5a36078ee5db05ca1ec0ff39d1812886954c836
rightlag <rightlag@gmail.com>
2017-08-28 13:42:26 -0400

    Add Vault integration for aplha deployment

--- mantis/requester.py
+++ mantis/requester.py
@@ -1,14 +1,30 @@
+import json
+import os
+import ssl
 import sys
+import tempfile
 import time
 
 from urllib.parse import urlparse
-from urllib.request import Request
+from urllib.request import build_opener, HTTPSHandler, Request, urlopen
 
 from . import CRLF, DEFAULT_CHARACTER_ENCODING
 from .command import HTTPCommand, RequestInvoker
 from .handler import DefaultRequestHandler
 from .loader import DefaultPackageLoader
 from .publisher import Publisher
+
+context = ssl.create_default_context()
+
+
+def initialize():
+    address = '{VAULT_ADDR}/v1/secret/password'.format(VAULT_ADDR=os.environ['VAULT_ADDR'])
+    request = Request(address, headers={'X-Vault-Token': os.environ['VAULT_TOKEN']})
+    with urlopen(request) as response, tempfile.NamedTemporaryFile() as f:
+        value = json.loads(response.read().decode(DEFAULT_CHARACTER_ENCODING))['data']['value']
+        f.write(value.encode(DEFAULT_CHARACTER_ENCODING))
+        f.seek(0)
+        context.load_cert_chain(f.name)
 
 
 class Requester:
@@ -23,6 +39,7 @@ class Requester:
 
     def request(self, duration=DEFAULT_TIMEOUT_INTERVAL):
         start = time.time()
+        initialize()
         arguments = self.arguments
         identifier = urlparse(arguments.address)
         path = self.loader.specification.paths[identifier.path]
@@ -31,14 +48,19 @@ class Requester:
         handler = self.loader.module.__dict__.get(
             operation.__dict__.get('x_request_handler', DefaultRequestHandler)
         )
-        cls = type(handler.__class__.__name__, (handler, DefaultRequestHandler,), {})
+        cls = type(handler.__class__.__name__, (handler, DefaultRequestHandler), {})
         invoker = RequestInvoker()
         invoker.register('onRequest', cls())
         while True:
             try:
-                command = HTTPCommand(Request(arguments.address, headers=arguments.header))
+                command = HTTPCommand(
+                    Request(arguments.address, headers=arguments.header, method=arguments.request),
+                    build_opener(HTTPSHandler(context=context))
+                )
                 invoker('onRequest', command)
-                self.publisher(message=command.response.body.encode(DEFAULT_CHARACTER_ENCODING))
+                instance = json.loads(command.response.body.decode(DEFAULT_CHARACTER_ENCODING))
+                print(json.dumps(instance, indent=2))
+                self.publisher(message=command.response.body)
                 time.sleep(duration)
             except KeyboardInterrupt:
                 elapsed = time.time() - start
